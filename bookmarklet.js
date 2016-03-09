0cf7708687beede4f260855ff7f9b240fd4b5038
James Cheese <trust@tr00st.co.uk>
2016-03-09 14:03:51 +0000

    Formatting for readability

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,22 @@
 (
     function() {
-        var styleCSS="*{background:rgba(0,0,0,0.02)!important;color:black!important;font-family:'Comic Sans MS',Arial!important;font-size:110%;padding:2px;margin:-3px;}input,textarea,select,button,a{border:1px dashed black!important;padding:2px;}body,html,div{padding:0;margin:0;}";
+        var styleCSS="\
+* {\
+    background:rgba(0,0,0,0.02)!important;\
+    color:black!important;\
+    font-family:'Comic Sans MS',Arial!important;\
+    font-size:110%;\
+    padding:2px;\
+    margin:-3px;\
+}\
+input,textarea,select,button,a {\
+    border:1px dashed black!important;\
+    padding:2px;\
+}\
+body,html,div{\
+    padding:0;\
+    margin:0;\
+}";
         var styleNode=document.createElement("style");
         styleNode.innerText=styleCSS;
         document.head.appendChild(styleNode);
